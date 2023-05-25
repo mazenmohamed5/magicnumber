c5b438fce0e84b001d4526a7823f04f308553210
mazenmohamed5 <88230604+mazenmohamed5@users.noreply.github.com>
2023-05-25 16:42:04 +0200

    Add files via upload

---
+++ magicnumber.py
@@ -0,0 +1,15 @@
+print("hello user")
+
+def magicnum():
+    
+    pickanum=input("please choose the magic number between 0 and 10\n")
+
+    if pickanum == "4":
+        print("good choice, you have won")
+
+    else:
+        print("oh sorry wrong choice , run the program again")    
+
+
+
+magicnum()        
